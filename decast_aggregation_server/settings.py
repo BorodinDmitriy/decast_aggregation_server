41db8ff3e5d19db2053a31d51df88cb7d8c3a63c
Borodin Dmitriy <demansen@yandex.ru>
2018-04-30 01:55:58 +0300

    fix settings

--- decast_aggregation_server/settings.py
+++ decast_aggregation_server/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 import djcelery
 djcelery.setup_loader()
@@ -185,3 +186,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
