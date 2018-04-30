f82cf307724c5ba5ea3e01d061a7a8e1cb8859a5
Borodin Dmitriy <demansen@yandex.ru>
2018-04-30 03:21:21 +0300

    revert test rate

--- server/views.py
+++ server/views.py
@@ -208,7 +208,7 @@ class GetPayBill(APIView):
     result = {}
     result["personal_account"] = "-"
     result["consumption_volume"] = "-"
-    result["rate"] = "15.34"
+    result["rate"] = "-"
     result["value"] = "-"
     
     #try:
